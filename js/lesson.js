8e1a88cd44c9e663f8de499d3c28f90830f5ef5f
Bekzat <apasniynegr228@gmail.com>
2023-12-02 01:22:13 +0600

    converter

--- js/lesson.js
+++ js/lesson.js
@@ -13,4 +13,67 @@ phoneButton.addEventListener('click', ()=>{
         phoneSpan.innerHTML = 'This number is False';
         phoneSpan.style.color = 'red';
     }
-})
+});
+
+document.getElementById('btn-convert').addEventListener('click', function() {
+    var somValue = document.getElementById('som').value;
+    var usdValue = document.getElementById('usd').value;
+    var eurValue = document.getElementById('eur').value;
+    var somInput= document.getElementById('som');
+    var usdInput = document.getElementById('usd');
+    var eurInput = document.getElementById('eur');
+
+
+    var som = parseFloat(somValue);
+    var usd = parseFloat(usdValue);
+    var eur = parseFloat(eurValue);
+
+    
+
+    var counter = 0;
+    let convertError = document.getElementById('convertError');
+    if (!isNaN(som)) {
+        counter++;
+    } if (!isNaN(usd)) {
+        counter++;
+    } if(!isNaN(eur)){
+        counter++;
+    }
+    if (counter === 0) {
+        convertError.textContent = "Введите одну валюту!";
+    } else if (counter != 1) {
+        convertError.textContent = "Введите только одну валюту!";
+    } else if (counter === 1){
+        if (!isNaN(som)) {
+            const result = convertFromSom(som);
+            usdInput.value = result.usd;
+            eurInput.value = result.eur; 
+        } else if (!isNaN(usd)) {
+            const result = convertFromUsd(usd);
+            somInput.value = result.som;
+            eurInput.value = result.eur;
+        } else if(!isNaN(eur)) {
+            const result = convertFromEur(eur) 
+            somInput.value = result.som;
+            usdInput.value = result.usd;
+            }
+        }
+    }
+);
+
+function convertFromSom(som) {
+    let usd = som * 0.011;
+    let eur = som * 0.010;
+    return {usd, eur};
+}
+function convertFromUsd(usd) {
+    let som = usd * 89;
+    let eur = usd * 0.92;
+    return {som, eur};
+}
+function convertFromEur(eur) {
+    let som = eur * 97;
+    let usd = eur * 1.09;
+    return {som, usd};
+}
+
