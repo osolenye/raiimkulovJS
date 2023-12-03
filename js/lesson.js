1212ea6473f3acb065792de612a6a715212c4ba3
Bekzat <apasniynegr228@gmail.com>
2023-12-04 00:39:04 +0600

    now

--- js/lesson.js
+++ js/lesson.js
@@ -19,6 +19,8 @@ document.getElementById('btn-convert').addEventListener('click', function() {
     var somValue = document.getElementById('som').value;
     var usdValue = document.getElementById('usd').value;
     var eurValue = document.getElementById('eur').value;
+
+
     var somInput= document.getElementById('som');
     var usdInput = document.getElementById('usd');
     var eurInput = document.getElementById('eur');
@@ -77,3 +79,50 @@ function convertFromEur(eur) {
     return {som, usd};
 }
 
+
+
+
+const slides = document.querySelectorAll('.slide')
+const next = document.querySelector('#btn-next')
+const prev = document.querySelector('#btn-prev')
+let index = 0
+
+const hideSlide = () => {
+    slides.forEach((slide) => {
+        slide.style.opacity = 0
+        slide.classList.remove('active_slide')
+    })
+}
+const showSlide = (i = 0) => {
+    slides[i].style.opacity = 1
+    slides[i].classList.add('active_slide')
+}
+
+hideSlide()
+showSlide(index)
+
+
+const autoSlider = (i = 0) => {
+    setInterval(() => {
+        i++
+        if (i > slides.length - 1) {
+            i = 0
+        }
+        hideSlide()
+        showSlide(i)
+    }, 10000)
+}
+
+next.onclick = () => {
+    index < slides.length - 1 ? index++ : index = 0
+    hideSlide()
+    showSlide(index)
+}
+
+prev.onclick = () => {
+    index > 0 ? index-- : index = slides.length - 1
+    hideSlide()
+    showSlide(index)
+}
+
+autoSlider(index)
